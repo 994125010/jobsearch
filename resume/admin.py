6ef7ff41c8367c7d378619272302ee786f8741dd
994125010 <li.yang.yi.994125010@gmail.com>
2016-03-16 19:36:57 -0700

    new lines

--- resume/admin.py
+++ resume/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Work, Project, Hobby, Course, Education
+from .models import *
 
-for item in (Work, Project, Hobby, Course, Education):
+for item in (Work, Project, Hobby, Course, Education, Skill):
     admin.site.register(item)
 # Register your models here.
